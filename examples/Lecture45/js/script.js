de4f2505bb41abda5baa5d108c6768cf143d7967
icysnake <icysnake1989@gmail.com>
2016-12-19 02:20:04 +0300

    some changes

--- examples/Lecture45/js/script.js
+++ examples/Lecture45/js/script.js
@@ -1,39 +1,68 @@
-// Object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "blue";
+// // Object creation
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "blue";
 
-console.log(company);
-console.log("Company CEO name is: " 
-  + company.ceo.firstName);
+// console.log(company);
+// console.log("Company CEO name is: " 
+//   + company.ceo.firstName);
 
-console.log(company["name"]);
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// console.log(company["name"]);
+// var stockPropName = "stock of company";
+// company[stockPropName] = 110;
 
-console.log("Stock price is: " + 
-  company[stockPropName]);
+// console.log("Stock price is: " + 
+//   company[stockPropName]);
 
-// Better way: object literal
-var facebook = {
-  name: "Facebook",
-  ceo: {
-    firstName: "Mark",
-    favColor: "blue"
-  },
-  "stock of company": 110
-};
+// // Better way: object literal
+// var facebook = {
+//   name: "Facebook",
+//   ceo: {
+//     firstName: "Mark",
+//     favColor: "blue"
+//   },
+//   "stock of company": 110
+// };
 
-console.log(facebook.ceo.firstName);
+// console.log(facebook.ceo.firstName);
 
+// var a = 0;
+// console.log(a + 1);
+// var element = new Object();
+// 	element.av = "Аудио и видео";
+// 	element.it = "Цифра";
+// 	element.it = new Object();
+// 	element.it.notebook = "Ноутбуки";
+// 	element.it.photo = "Фотоаппараты";
+// 	console.log(element);
+// 	console.log("В IT отделе имеются " + element.it.notebook + " и " + element.it.photo + ".");
 
+var el = {
+	otdel: {
+	oav: "АВ",
+		oit: "Цифра",
+		it: {
+			notebook: "Ноутбуки",
+			phone: "Телефоны",
+			photo: "Фотоаппараты",
+			komp: "Компьютеры"
+		},
+		av: {
+			tv: "Телевизоры",
+			audio: "Аудиотехника",
+			auto: "Автомобильная техника"
+		},
+		ombt: "МБТ",
+		okbt: "КБТ"
+	},
+	"plan of cell": 800000
+}
 
-
-
-
-
-
+console.log("Отделы 5 элемента: " + el.otdel.oit + ", " + el.otdel.oav + ", " + el.otdel.ombt + " и " + 
+	el.otdel.okbt + ".");
+console.log("В ИТ отделе продаются: " + el.otdel.it.notebook + ", " + el.otdel.it.phone
+ + ", " + el.otdel.it.photo + ", " + el.otdel.it.komp + ".")
 
 
--- examples/Lecture49/js/script.js
+++ examples/Lecture49/js/script.js
@@ -1,22 +1,53 @@
-// Object literals and "this"
-var literalCircle = {
-  radius: 10,
+// // Object literals and "this"
+// // var literalCircle = {
+// //   radius: 10,
 
-  getArea: function () {
-    var self = this;
-    console.log(this);
+// //   getArea: function () {
+// //     var self = this;
+// //     console.log(this);
 
-    var increaseRadius = function () {
-      self.radius = 20;
-    };
-    increaseRadius();
-    console.log(this.radius);
+// //     var increaseRadius = function () {
+// //       self.radius = 20;
+// //     };
+// //     increaseRadius();
+// //     console.log(this.radius);
 
-    return Math.PI * Math.pow(this.radius, 2);
-  }
+// //     return Math.PI * Math.pow(this.radius, 2);
+// //   }
+// // };
+
+// // console.log(literalCircle.getArea());
+
+// function Dog(name) {
+//   this.name = name;
+// }
+// Dog.prototype.bark = function() {
+//   console.log(this.name + " likes barking! Bark!");
+//   console.log(this);
+// };
+
+
+// var max = new Dog("Max");
+// var min = new Dog("Buddy");
+
+// max.bark();
+// min.bark();
+
+// (function iife (iifearg) {
+//   var results = 5;
+//   this.results = results + iifearg;
+//   console.log(0 + " , " + this.results);
+//   console.log(this)
+// })(23)
+
+(function(window) {
+var obj = {};
+obj.dreamOn = function () {
+ console.log("I want to see the global scope! Let me out!");
 };
 
-console.log(literalCircle.getArea());
+window.doer = obj;
 
+})(window);
 
-
+doer.dreamOn();
